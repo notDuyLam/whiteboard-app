0e6e0fe6ed44ef9cab93e9b114b86ecfd12812aa
notDuyLam <22120181@student.hcmus.edu.vn>
2025-12-06 16:13:32 +0700

    feat(ui): add NavigationView with menu items, icons, GoBack functionality and responsive behavior

--- whiteboard-app/MainWindow.xaml.cs
+++ whiteboard-app/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Graphics;
+using whiteboard_app.Services;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,12 +27,15 @@ namespace whiteboard_app
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private INavigationService? _navigationService;
+
         public MainWindow()
         {
             InitializeComponent();
             ExtendsContentIntoTitleBar = true;
             SetTitleBar(AppTitleBar);
             InitializeTitleBar();
+            InitializeNavigation();
         }
 
         private void InitializeTitleBar()
@@ -42,6 +46,41 @@ namespace whiteboard_app
                 titleBar.ExtendsContentIntoTitleBar = true;
                 titleBar.ButtonBackgroundColor = Colors.Transparent;
                 titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            }
+        }
+
+        private void InitializeNavigation()
+        {
+            if (App.ServiceProvider != null)
+            {
+                _navigationService = App.ServiceProvider.GetService<INavigationService>();
+                _navigationService?.SetNavigationFrame(ContentFrame);
+            }
+        }
+
+        private void RootNavigationView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (_navigationService?.CanGoBack == true)
+            {
+                _navigationService.GoBack();
+            }
+        }
+
+        private void RootNavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.InvokedItemContainer is NavigationViewItem item && item.Tag is string tag)
+            {
+                // Navigation will be handled by ViewModels
+                // This is just the UI structure
+            }
+        }
+
+        private void RootNavigationView_DisplayModeChanged(NavigationView sender, NavigationViewDisplayModeChangedEventArgs args)
+        {
+            // Responsive behavior: adjust pane display mode based on window size
+            if (args.DisplayMode == NavigationViewDisplayMode.Compact || args.DisplayMode == NavigationViewDisplayMode.Minimal)
+            {
+                // Compact mode - pane can be toggled
             }
         }
     }
